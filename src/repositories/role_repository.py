d83cf448f472d2f7bc5dd1ad5e55b49cb2b3c2a9
Tuomas Alanen <tualanen@cs.helsinki.fi>
2022-01-05 17:42:54 +0200

    roles refactored

--- src/repositories/role_repository.py
+++ src/repositories/role_repository.py
@@ -3,33 +3,96 @@ from utils.exceptions import DatabaseException, NotExistingException
 
 
 class RoleRepository:
+    '''Class for handling Roles in the database
+    '''
 
-    def get_all(self):
-        sql = "SELECT id, name, description FROM Roles"
+    def get_all(self) -> [tuple]:
+        '''get_all is used to list of all roles
+
+        If no roles found, returns empty list.
+
+        Raises:
+            DatabaseException: raised if problems occur
+                while interacting with the database
+
+        Returns:
+            [tuple]: list of all roles
+        '''
+
+        sql = '''
+            SELECT id, name, description 
+            FROM Roles
+        '''
+
         try:
             roles = db.session.execute(sql).fetchall()
         except Exception as error:
-            raise DatabaseException('while getting all roles') from error
+            raise DatabaseException('While getting all roles') from error
 
         return [(role[0], role[1], role[2]) for role in roles]
 
-    def get_by_id(self, rid: str):
-        sql = "SELECT id, name, description FROM Roles"
+    def get_by_id(self, rid: int) -> tuple:
+        '''get_by_id is used to found role with given id
+
+        Args:
+            rid (str): id of the role to be found
+
+        Raises:
+            DatabaseException: raised if problems while
+                interacting with the database
+            NotExistingException: raised if role is not
+                found with given id
+
+        Returns:
+            tuple: role with given id
+        '''
+
+        sql = '''
+            SELECT id, name, description 
+            FROM Roles
+        '''
+
         try:
-            role = db.session.execute(sql, {"id": rid}).fetchone()
+            role = db.session.execute(sql, {'id': rid}).fetchone()
         except Exception as error:
-            raise DatabaseException('while getting roles') from error
+            raise DatabaseException('While getting the role') from error
+
+        if not role:
+            raise NotExistingException('Role')
 
         return (role[0], role[1], role[2])
 
-    def get_name(self, rid: str):
-        sql = "SELECT name FROM Roles WHERE id=:id"
-        try:
-            name = db.session.execute(sql, {"id": rid}).fetchone()[0]
-        except Exception as error:
-            raise DatabaseException('while getting role') from error
+    def get_name(self, rid: int) -> str:
+        '''get_name is used to get name of role with given id
 
-        return name
+        Args:
+            rid (int): id of role
+
+        Raises:
+            DatabaseException: raised if problems while
+                interacting with the database
+            NotExistingException: raised if role is not
+                found with given id
+
+        Returns:
+            str: found name
+        '''
+
+        sql = '''
+            SELECT name 
+            FROM Roles 
+            WHERE id=:id
+        '''
+
+        try:
+            name = db.session.execute(sql, {'id': rid}).fetchone()
+        except Exception as error:
+            raise DatabaseException("While getting role's name") from error
+
+        if not name:
+            raise NotExistingException('Role')
+
+        return name[0]
 
 
 role_repository = RoleRepository()
--- src/services/role_service.py
+++ src/services/role_service.py
@@ -1,4 +1,5 @@
 from repositories.role_repository import role_repository, RoleRepository
+from utils.exceptions import UnvalidInputException
 
 
 class RoleService:
@@ -7,13 +8,66 @@ class RoleService:
                  default_role_repository: RoleRepository = role_repository):
         self._role_repository = default_role_repository
 
-    def get_all(self):
+    def get_all(self) -> [tuple]:
+        '''get_all is used to list of all roles
+
+        If no roles found, returns empty list.
+
+        Raises:
+            DatabaseException: raised if problems occur
+                while interacting with the database
+
+        Returns:
+            [tuple]: list of all roles
+        '''
         return self._role_repository.get_all()
 
-    def get_by_id(self, rid: str):
+    def get_by_id(self, rid: str) -> tuple:
+        '''get_by_id is used to found role with given id
+
+        Args:
+            rid (str): id of the role to be found
+
+        Raises:
+            DatabaseException: raised if problems while
+                interacting with the database
+            NotExistingException: raised if role is not
+                found with given id
+
+        Returns:
+            tuple: role with given id
+        '''
+
+        try:
+            rid = int(rid)
+        except Exception as error:
+            raise UnvalidInputException(reason='unvalid formatting of id',
+                                        source='role') from error
+
         return self._role_repository.get_by_id(rid)
 
-    def get_name(self, rid: str):
+    def get_name(self, rid: str) -> str:
+        '''get_name is used to get name of role with given id
+
+        Args:
+            rid (int): id of role
+
+        Raises:
+            DatabaseException: raised if problems while
+                interacting with the database
+            NotExistingException: raised if role is not
+                found with given id
+
+        Returns:
+            str: found name
+        '''
+
+        try:
+            rid = int(rid)
+        except Exception as error:
+            raise UnvalidInputException(reason='unvalid formatting of id',
+                                        source='role') from error
+
         return self._role_repository.get_name(rid)
 
 
