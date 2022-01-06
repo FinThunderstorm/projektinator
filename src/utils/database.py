6a4aa39f30f9c20bacc45f655ffb53b1ad8fca7a
Tuomas Alanen <tualanen@cs.helsinki.fi>
2022-01-06 21:03:43 +0200

    heroku postgres connection fix

--- src/utils/database.py
+++ src/utils/database.py
@@ -2,8 +2,9 @@ from app import app
 from flask_sqlalchemy import SQLAlchemy
 import utils.config as configs
 
-print(configs.database_url)
-
+db_uri = configs.database_url
+if db_uri.startswith("postgres://"):
+    db_uri = db_uri.replace("postgres://", "postgresql://", 1)
 app.config['SQLALCHEMY_DATABASE_URI'] = configs.database_url
 # let's get rid of annoying warning in the logs.
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
