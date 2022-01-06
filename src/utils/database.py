9bcc4d8fb791d81d87361d412293b3084b7190f1
Tuomas Alanen <tualanen@cs.helsinki.fi>
2022-01-06 21:07:37 +0200

    try again to fix

--- src/utils/database.py
+++ src/utils/database.py
@@ -3,9 +3,13 @@ from flask_sqlalchemy import SQLAlchemy
 import utils.config as configs
 
 db_uri = configs.database_url
-if db_uri.startswith("postgres://"):
-    db_uri = db_uri.replace("postgres://", "postgresql://", 1)
-app.config['SQLALCHEMY_DATABASE_URI'] = configs.database_url
+
+if db_uri.startswith('postgres://'):
+    db_uri = db_uri.replace('postgres://', 'postgresql://', 1)
+
+print(db_uri)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
 # let's get rid of annoying warning in the logs.
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
